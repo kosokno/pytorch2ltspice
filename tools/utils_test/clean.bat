96672ddd016f3f0654633c24705808dcdf512692
kosokno <kos.okuno@gmail.com>
2025-12-28 18:37:10 +0900

    updated clean.bat in utils_test

--- tools/utils_test/clean.bat
+++ tools/utils_test/clean.bat
@@ -2,13 +2,14 @@
 choice /c YN /m "Are you sure you want to delete all the simulation artifacts? (Y/N): "
 if errorlevel 2 goto end
 
-echo Deleting *.py, *.sp, *.txt, and 'env\__pycache__' and 'tmp' folder...
-if exist "env\*.py" del /Q "env\*.py" 
-if exist "env\*.sp" del /Q "env\*.sp" 
-if exist "env\*.txt" del /Q "env\*.txt" 
+echo Deleting *.py, *.sp, *.txt, and 'gym\__pycache__' and 'tmp' folder...
+if exist "gym\*.py" del /Q "gym\*.py" 
+if exist "gym\*.sp" del /Q "gym\*.sp" 
+if exist "gym\*.txt" del /Q "gym\*.txt" 
+if exist "gym\sig_gen*" del /Q "gym\sig_gen*" 
 
 if exist "tmp" rmdir /S /Q "tmp"
-if exist "env\__pycache__" rmdir /S /Q "env\__pycache__"
+if exist "gym\__pycache__" rmdir /S /Q "gym\__pycache__"
 
 
 echo Deletion complete.
