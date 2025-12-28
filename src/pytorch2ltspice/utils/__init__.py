3a0617083c206f30ee79fb814a84e99a56f1c417
kosokno <kos.okuno@gmail.com>
2025-12-28 15:18:53 +0900

    added sample on clock

--- src/pytorch2ltspice/utils/__init__.py
+++ src/pytorch2ltspice/utils/__init__.py
@@ -1,3 +1,7 @@
 from .siggen import generate_siggen_asc_asy
+from .clock_sampling import sample_on_clock
 
-__all__ = ["generate_siggen_asc_asy"]
+__all__ = [
+    "generate_siggen_asc_asy",
+    "sample_on_clock",
+]
